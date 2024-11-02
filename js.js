3ec0fadbeb2dd445adacc7854e908d4ab5570954
Hamish M <162705893+HamishMonke@users.noreply.github.com>
2024-11-02 13:07:53 +0930

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+function gamesFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 1250;
+}
+
+function aboutusFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 2500;
+}
+
+function teamFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 3600;
+}
+
+function contactandlegalFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 10000;
+}
