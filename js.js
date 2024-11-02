7c88d43b61df983aa686d57e83d78a9fabf34ddf
Hamish M <hamish4utube06@gmail.com>
2024-11-02 16:55:37 +0930

    c

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 function ScrollTo(elementName) {
     console.log("Element name:", elementName); // Check if elementName is correct
-    //var elements = document.getElementsByClassName(elementName);
-    //if (elements.length > 0) {
-    //    elements[0].scrollIntoView();
-    //}
+    var elements = document.getElementsByClassName(elementName);
+    if (elements.length > 0) {
+        elements[0].scrollIntoView();
+    }
 }
