9586ddd7c7ac6527764da6d64c2aa9763a6e56dd
Hamish M <hamish4utube06@gmail.com>
2024-11-02 15:48:36 +0930

    change

--- js.js
+++ js.js
@@ -1,20 +1,3 @@
-function gamesFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 1250;
-}
-
-function aboutusFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 2500;
-}
-
-function teamFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 3600;
-}
-
-function contactandlegalFunction() {
-    document.getElementById('contact').scrollIntoView();
-    //document.body.scrollTop = 0;
-    //document.documentElement.scrollTop = 10000;
+function ScrollTo(elementName) {
+    document.getElementById(elementName).scrollIntoView();
 }
