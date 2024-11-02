6a6aeb12610a61130fb6d92dfc19a96dfebe5cb4
Hamish M <hamish4utube06@gmail.com>
2024-11-02 16:25:27 +0930

    c

--- js.js
+++ js.js
@@ -1,12 +1,6 @@
 function ScrollTo(elementName) {
-    var element = document.getElementsByClassName(elementName)
-
-    if(element != null){
-        console.log("Found element")
-
+    var element = document.getElementsByClassName(elementName)[0];
+    if (element) {
         element.scrollIntoView();
-    }
-    else{
-        console.log("Could not find element")
     }
 }
