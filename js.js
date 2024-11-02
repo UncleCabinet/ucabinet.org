cee1355e99da262447961c943b33aa9922f40b30
Hamish M <hamish4utube06@gmail.com>
2024-11-02 16:36:22 +0930

    c

--- js.js
+++ js.js
@@ -1,10 +1,7 @@
 function ScrollTo(elementName) {
     console.log("Element name:", elementName); // Check if elementName is correct
-    var elements = document.getElementsByClassName(elementName);
-    console.log("Elements found:", elements); // See if any elements are found
-    if (elements.length > 0) {
-        elements[0].scrollIntoView();
-    } else {
-        console.error("Element with the specified class not found:", elementName);
-    }
+    //var elements = document.getElementsByClassName(elementName);
+    //if (elements.length > 0) {
+    //    elements[0].scrollIntoView();
+    //}
 }
